4127ffa03117c0784071539fab4085ea11ae8587
Duc Filan <ducfilan@gmail.com>
2016-09-07 14:35:03 +0700

    Revert master

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,46 +1,78 @@
 //@ sourceMappingURL=contentscript.map
 (function() {
-  var excludeRooms, handleDOM, timer;
+    var excludeRooms, handleDOM, timer, unreadRoomsName;
 
-  excludeRooms = [];
+    excludeRooms = [];
+    unreadRoomsName = [];
 
-  timer = 0;
+    timer = 0;
 
-  handleDOM = function() {
-    if (timer) {
-      return;
+    getMentionCounts = function() {
+        return $("#_roomListArea .mention").length;
     }
-    timer = setTimeout(function() {
-      $.each(excludeRooms, function(_, roomId) {
-        var badgeDomObj, domObj, selector;
 
-        selector = "#_roomListItems li[data-rid=" + roomId + "]";
-        domObj = $(selector);
-        if (domObj.length === 1) {
-          domObj = domObj[0];
-          $(domObj).removeClass("roomUnread");
-          badgeDomObj = $(domObj).find('.chatListMeta ul.incomplete');
-          if (badgeDomObj.length === 1) {
-            return badgeDomObj[0].remove();
-          }
+    getUnreadRoomsName = function() {
+        var l_unreadRoomsName = $.map($("#_roomListItems .roomUnread .chatListTitleArea"), $.text);
+
+        if (l_unreadRoomsName.length == 0) {
+            l_unreadRoomsName = ["No message!"];
         }
-      });
-      return timer = 0;
-    }, 5);
-    return null;
-  };
 
-  $(document).ready(function() {
-    return chrome.runtime.sendMessage({
-      mode: "initialize"
-    }, function(response) {
-      if (response.status === "success") {
-        excludeRooms = response.options.excludeRooms;
-        return $("#_roomListArea").on("DOMNodeInserted", function(event) {
-          return handleDOM();
+        return l_unreadRoomsName;
+    }
+
+    setCustomTitle = function() {
+        document.title = "[" + unreadRoomsName.length + "](" + getMentionCounts() + ") - " + unreadRoomsName.join("|");
+    }
+
+    handleDOM = function() {
+        if (timer) {
+            return;
+        }
+        timer = setTimeout(function() {
+            unreadRoomsName = getUnreadRoomsName();
+
+            $.each(excludeRooms, function(_, roomId) {
+                var badgeDomObj, domObj, selector;
+
+                selector = "#_roomListItems li[data-rid=" + roomId + "]";
+                domObj = $(selector);
+                if (domObj.length === 1) {
+                    domObj = domObj[0];
+
+                    unreadRoomsName.splice(unreadRoomsName.indexOf($(selector + " .chatListTitleArea").text()), 1);
+
+                    $(domObj).removeClass("roomUnread");
+                    badgeDomObj = $(domObj).find('.chatListMeta ul.incomplete');
+                    if (badgeDomObj.length === 1) {
+                        return badgeDomObj[0].remove();
+                    }
+                }
+            });
+
+            setCustomTitle();
+
+            return timer = 0;
+        }, 5);
+        return null;
+    };
+
+    $(document).ready(function() {
+        return chrome.runtime.sendMessage({
+            mode: "initialize"
+        }, function(response) {
+            if (response.status === "success") {
+                excludeRooms = response.options.excludeRooms;
+
+                unreadRoomsName = getUnreadRoomsName();
+
+                setCustomTitle();
+
+                return $("#_roomListArea").on("DOMNodeInserted", function(event) {
+                    return handleDOM();
+                });
+            }
         });
-      }
     });
-  });
 
 }).call(this);
