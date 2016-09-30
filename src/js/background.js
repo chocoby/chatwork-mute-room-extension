b26589b4b2695043f2d80e890ef95d79fc93ab17
Kenta Okamoto <chocoby@gmail.com>
2016-10-01 01:32:32 +0900

    Rewrite in ES2015

--- src/js/background.js
+++ src/js/background.js
@@ -1,45 +1,38 @@
-(function() {
-  var loadOptions, saveOptions;
+'use strict';
 
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    var options;
-    switch (request.mode) {
-      case "initialize":
-        options = loadOptions();
-        sendResponse({
-          status: "success",
-          options: options
-        });
-        break;
-      case "loadOptions":
-        options = loadOptions();
-        sendResponse({
-          status: "success",
-          options: options
-        });
-        break;
-      case "saveOptions":
-        saveOptions(request.options);
-        sendResponse({
-          status: "success"
-        });
-        break;
-      default:
-        console.error("Undefined function: " + request.mode);
-    }
-    return true;
-  });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  let options;
+  switch (request.mode) {
+    case "initialize":
+      options = loadOptions();
+      sendResponse({
+        status: "success",
+        options: options
+      });
+      break;
+    case "loadOptions":
+      options = loadOptions();
+      sendResponse({
+        status: "success",
+        options: options
+      });
+      break;
+    case "saveOptions":
+      saveOptions(request.options);
+      sendResponse({
+        status: "success"
+      });
+      break;
+    default:
+      console.error("Undefined function: " + request.mode);
+  }
+});
 
-  saveOptions = function(options) {
-    var option_json;
-    option_json = JSON.stringify(options);
-    return localStorage.setItem("options", option_json);
-  };
+function saveOptions(options) {
+  let optionJson = JSON.stringify(options);
+  localStorage.setItem("options", optionJson);
+};
 
-  loadOptions = function() {
-    return JSON.parse(localStorage.getItem("options"));
-  };
-
-}).call(this);
-
-//# sourceMappingURL=background.js.map
+function loadOptions() {
+  return JSON.parse(localStorage.getItem("options"));
+};
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,47 +1,40 @@
-(function() {
-  $("#add").click(function() {
-    return $("#roomIds").append('<div class="room"><input type="text" size="15" placeholder="1234567"><button class="remove">-</button>');
+'use strict';
+
+$("#add").click(() => {
+  $("#roomIds").append('<div class="room"><input type="text" size="15" placeholder="1234567"><button class="remove">-</button>');
+});
+
+$("#save").click(() => {
+  let excludeRooms = [];
+  $("#roomIds .room").each((_, room) => {
+    const roomId = $(room).children("input").val();
+    if (roomId.length > 0) {
+      excludeRooms.push(roomId);
+    }
   });
 
-  $("#save").click(function() {
-    var excludeRooms, options;
-    excludeRooms = [];
-    $("#roomIds .room").each(function(_, room) {
-      var roomId;
-      roomId = $(room).children("input").val();
-      if (roomId.length > 0) {
-        return excludeRooms.push(roomId);
-      }
-    });
-    options = {
-      excludeRooms: excludeRooms
-    };
-    return chrome.runtime.sendMessage({
-      mode: "saveOptions",
-      options: options
-    }, function(response) {
-      return $("#status").text("Saved.");
-    });
+  const options = {
+    excludeRooms: excludeRooms
+  };
+  chrome.runtime.sendMessage({
+    mode: "saveOptions",
+    options: options
+  }, (response) => {
+    $("#status").text("Saved.");
   });
+});
 
-  $(document).on('click', '.remove', function(event) {
-    return $(event.currentTarget).parent().remove();
+$(document).on('click', '.remove', event => {
+  $(event.currentTarget).parent().remove();
+});
+
+$(document).ready(function() {
+  chrome.runtime.sendMessage({ mode: "loadOptions" }, response => {
+    const options = response.options;
+    if (options && options.excludeRooms) {
+      $.each(options.excludeRooms, (_, room) => {
+        $("#roomIds").append(`<div class="room"><input type="text" size="15" placeholder="1234567" value="${room}"><button class="remove">-</button>`);
+      });
+    }
   });
-
-  $(document).ready(function() {
-    return chrome.runtime.sendMessage({
-      mode: "loadOptions"
-    }, function(response) {
-      var options;
-      options = response.options;
-      if (options && options.excludeRooms) {
-        return $.each(options.excludeRooms, function(_, room) {
-          return $("#roomIds").append('<div class="room"><input type="text" size="15" placeholder="1234567" value="' + room + '"><button class="remove">-</button>');
-        });
-      }
-    });
-  });
-
-}).call(this);
-
-//# sourceMappingURL=popup.js.map
+});
