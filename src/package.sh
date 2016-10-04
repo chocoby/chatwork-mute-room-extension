0f246928b3795be05292c389140ffbcc0098d28e
Kenta Okamoto <chocoby@gmail.com>
2016-10-04 23:30:34 +0900

    Edit extension's name

--- src/package.sh
+++ src/package.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-package_dir=../chatwork-mute-room
+cd ../
+package_dir=chatwork-mute-room
+zip_dir=chatwork-mute-room.zip
+
+rm -r $package_dir
+rm $zip_dir
 mkdir $package_dir
-cp manifest.json $package_dir
-cp popup.html $package_dir
-cp -r build $package_dir
+cp src/manifest.json $package_dir
+cp src/popup.html $package_dir
+cp -r src/build $package_dir
+zip -r $zip_dir $package_dir
